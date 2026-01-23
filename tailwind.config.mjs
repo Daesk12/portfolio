a0547284e2ac8ab77bc5992a0435960c045ea222
Daesk12 <danielesco2003@gmail.com>
2026-01-23 14:55:10 -0500

    Header terminado

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -54,6 +54,7 @@ export default {
       },
 
       fontSize: {
+        headertitle: ['clamp(8rem, 15vw, 16rem)', { lineHeight: '1' }],
         h1: ['clamp(2.5rem, 6vw, 4rem)', { lineHeight: '1.1' }],
         h2: ['clamp(2rem, 5vw, 3rem)', { lineHeight: '1.15' }],
         h3: ['clamp(1.5rem, 3.5vw, 2rem)', { lineHeight: '1.25' }],
