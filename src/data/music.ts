fee34d1b2bb4de922867b28b8c0c03ebde2e5571
Daesk12 <danielesco2003@gmail.com>
2026-02-04 02:07:59 -0500

    Nav abierto por defecto / theme dark-light

--- src/data/music.ts
+++ src/data/music.ts
@@ -5,6 +5,8 @@ export interface Cancion {
   id: number;
   waveColor: string;
   progressColor: string;
+  waveColorLight?: string;
+  progressColorLight?: string;
 }
 
 export const canciones: Cancion[] = [
@@ -15,6 +17,8 @@ export const canciones: Cancion[] = [
     image: '/images/about/01.webp',
     waveColor: '#eccc68',
     progressColor: '#ffa502',
+    waveColorLight: '#d4b657',
+    progressColorLight: '#e69500',
   },
   {
     id: 2,
@@ -23,6 +27,8 @@ export const canciones: Cancion[] = [
     image: '/images/about/02.webp',
     waveColor: '#a4b0be',
     progressColor: '#3742fa',
+    waveColorLight: '#8a96a4',
+    progressColorLight: '#2a35e0',
   },
   {
     id: 3,
@@ -31,5 +37,7 @@ export const canciones: Cancion[] = [
     image: '/images/about/03.webp',
     waveColor: '#ffb8b8',
     progressColor: '#ff3838',
+    waveColorLight: '#e6a6a6',
+    progressColorLight: '#e62e2e',
   },
 ];
